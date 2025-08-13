42719fb8454ddb672485e4b28d8b1451af2c9405
Lucca Giovanni Lobo Gonçalves <luccalobo.goncalves@gmail.com>
2025-08-13 01:48:26 -0300

    fix: removed non existent auth module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,9 @@ import { AppService } from './app.service';
 import { ClientsModule } from './modules/clients/clients.module';
 import { PrismaModule } from './core/database/prisma.module';
 import { SalesModule } from './modules/sales/sales.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ClientsModule, PrismaModule, SalesModule, AuthModule],
+  imports: [ClientsModule, PrismaModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
